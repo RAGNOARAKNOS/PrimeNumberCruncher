d7d3ac99c367df65c7fe576bf09a78f1059b672b
RAGNOARAKNOS <ragnocorp@googlemail.com>
2016-02-02 18:59:04 +0000

    It calculates primes!

--- PrimeCrunch/Crunch/Program.cs
+++ PrimeCrunch/Crunch/Program.cs
@@ -10,6 +10,46 @@ namespace Crunch
     {
         static void Main(string[] args)
         {
+            int primeSum = 0;
+
+            for (int i = 1, primeCounter = 0; primeCounter < 1000; i++)
+            {
+                if(!IsPrime(i))
+                {
+                    continue;
+                }
+                else
+                {
+                    primeCounter++;
+                    primeSum += i;
+                }
+            }
+
+            Console.WriteLine(primeSum);
+            Console.Read();
+        }
+
+        static bool IsPrime(int number)
+        {
+            if(number == 1)
+            {
+                return false;
+            }
+
+            if (number == 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i <= Math.Ceiling(Math.Sqrt(number)); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
